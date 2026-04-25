1ae66a9661e43e4b10aecefddbb600f4dbc1a202
ravijeeva01bci27-cell <ravijeeva01_bci27@mepcoeng.ac.in>
2026-04-25 10:20:47 +0530

    Sorting Array of Strings.c

--- Sorting Array of Strings.c
+++ Sorting Array of Strings.c
@@ -1,58 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// 1. Lexicographically non-decreasing
 int lexicographic_sort(const char* a, const char* b) {
-
+    return strcmp(a, b);
 }
 
+// 2. Lexicographically non-increasing
 int lexicographic_sort_reverse(const char* a, const char* b) {
-
+    return strcmp(b, a);
 }
 
-int sort_by_number_of_distinct_characters(const char* a, const char* b) {
-    
-}
-
-int sort_by_length(const char* a, const char* b) {
-
-}
-
-void string_sort(char** arr,const int len,int (*cmp_func)(const char* a, const char* b)){
-
-}
-
-
-int main() 
-{
-    int n;
-    scanf("%d", &n);
-  
-    char** arr;
-	arr = (char**)malloc(n * sizeof(char*));
-  
-    for(int i = 0; i < n; i++){
-        *(arr + i) = malloc(1024 * sizeof(char));
-        scanf("%s", *(arr + i));
-        *(arr + i) = realloc(*(arr + i), strlen(*(arr + i)) + 1);
+// Helper to count distinct characters
+int count_distinct(const char* s) {
+    int count = 0;
+    int hash[26] = {0};
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (hash[s[i] - 'a'] == 0) {
+            hash[s[i] - 'a'] = 1;
+            count++;
+        }
     }
-  
-    string_sort(arr, n, lexicographic_sort);
-    for(int i = 0; i < n; i++)
-        printf("%s\n", arr[i]);
-    printf("\n");
+    return count;
+}
 
-    string_sort(arr, n, lexicographic_sort_reverse);
-    for(int i = 0; i < n; i++)
-        printf("%s\n", arr[i]); 
-    printf("\n");
+// 3. Sort by number of distinct characters
+int sort_by_number_of_distinct_characters(const char* a, const char* b) {
+    int res = count_distinct(a) - count_distinct(b);
+    return (res == 0) ? lexicographic_sort(a, b) : res;
+}
 
-    string_sort(arr, n, sort_by_length);
-    for(int i = 0; i < n; i++)
-        printf("%s\n", arr[i]);    
-    printf("\n");
+// 4. Sort by length
+int sort_by_length(const char* a, const char* b) {
+    int res = strlen(a) - strlen(b);
+    return (res == 0) ? lexicographic_sort(a, b) : res;
+}
 
-    string_sort(arr, n, sort_by_number_of_distinct_characters);
-    for(int i = 0; i < n; i++)
-        printf("%s\n", arr[i]); 
-    printf("\n");
+// The generic sorting function
+void string_sort(char** arr, const int len, int (*cmp_func)(const char* a, const char* b)) {
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            // If cmp_func returns > 0, it means arr[i] > arr[j] based on our logic
+            if (cmp_func(arr[i], arr[j]) > 0) {
+                char* temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
